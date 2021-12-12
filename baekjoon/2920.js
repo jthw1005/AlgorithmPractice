38ebb5fcf323464bad5510766b04cae7d4e37e61
jthw1005 <skl0105@naver.com>
2021-12-12 17:36:00 +0900

    2920 done

---
+++ baekjoon/2920.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (input) {
+  const arr = input.split(" ");
+
+  let dx = -1;
+  let anwser = "descending";
+  if (Number(arr[0]) === 1) {
+    dx = 1;
+    anwser = "ascending";
+  }
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (Number(arr[i + 1]) !== Number(arr[i]) + dx) {
+      console.log("mixed");
+      rl.close();
+      return false;
+    }
+  }
+
+  console.log(`${anwser}`);
+  rl.close();
+});
+
+rl.on("close", function () {
+  process.exit();
+});
