f993a6db69af3cca189ae0ca54d1da7e9b396934
jthw1005 <skl0105@naver.com>
2022-01-16 23:52:13 +0900

    1076 풀이완료

---
+++ baekjoon/1076.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./1076.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const table = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+console.log((table[input[0]] * 10 + table[input[1]]) * 10 ** table[input[2]]);
