2eed47a4cf8833ad984220c2c47f504497731dd0
jthw1005 <skl0105@naver.com>
2022-01-16 23:48:44 +0900

    1009 풀이완료

---
+++ baekjoon/1009.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./1009.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const testCaseNum = +input[0];
+
+for (let i = 1; i <= testCaseNum; i++) {
+  const arr = input[i].split(" ").map((item) => +item);
+
+  const result = [];
+  const a = arr[0] % 10;
+  const b = arr[1];
+  let tmp = a;
+  let cnt = 0;
+
+  do {
+    result.push(tmp);
+    tmp *= a;
+    tmp %= 10;
+    cnt++;
+  } while (tmp !== a);
+
+  let d = b % cnt;
+  if (d === 0) d = cnt;
+
+  console.log(result[d - 1] === 0 ? 10 : result[d - 1]);
+}
