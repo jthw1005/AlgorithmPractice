40e66a53e885b61116372b850cac75fd918558d3
jthw1005 <skl0105@naver.com>
2022-09-11 21:54:08 +0900

    Update isSubsequencs.js

--- udemy/isSubsequence.js
+++ udemy/isSubsequence.js
@@ -3,14 +3,10 @@ function isSubsequence(str1, str2) {
   let rightPointer = 0;
 
   for (; leftPointer < str1.length; leftPointer++) {
-    if (str1[leftPointer] === str2[rightPointer]) {
-      leftPointer++;
-      rightPointer++;
-    } else if (str1[leftPointer] !== str2[rightPointer]) {
-      rightPointer++;
-    }
+    if (str1[leftPointer] === str2[rightPointer]) leftPointer++;
+    rightPointer++;
   }
 
   if (leftPointer === str1.length) return true;
-  else return false;
+  return false;
 }
