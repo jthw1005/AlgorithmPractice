303971892e37c1fd08a66ae2fcc58fb273b56f14
jthw1005 <skl0105@naver.com>
2022-09-20 01:14:39 +0900

    Solve nestedEvenSum.js

---
+++ udemy/Recursive/nestedEvenSum.js
@@ -0,0 +1,30 @@
+function nestedEvenSum(obj) {
+  let evenSum = 0;
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === 'object') evenSum += nestedEvenSum(obj[key]);
+    else if (typeof obj[key] === 'number' && obj[key] % 2 === 0) evenSum += obj[key];
+  });
+  return evenSum;
+}
+
+const obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: 'yup',
+    },
+  },
+};
+
+const obj2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+  d: 1,
+  e: { e: { e: 2 }, ee: 'car' },
+};
+
+console.log(nestedEvenSum(obj2));
