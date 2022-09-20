ef4e3912272e5b5fd6ffc229076eae0634432a45
jthw1005 <skl0105@naver.com>
2022-09-20 14:58:10 +0900

    Solve collectStrings.js

---
+++ udemy/Recursive/collectStrings.js
@@ -0,0 +1,29 @@
+function collectStrings(obj) {
+  const result = [];
+
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === 'string') result.push(obj[key]);
+    else if (typeof obj[key] === 'object' && !Array.isArray(obj))
+      result.push(...collectStrings(obj[key]));
+  });
+
+  return result;
+}
+
+const obj = {
+  stuff: 'foo',
+  data: {
+    val: {
+      thing: {
+        info: 'bar',
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: 'baz',
+          },
+        },
+      },
+    },
+  },
+};
+
+console.log(collectStrings(obj));
