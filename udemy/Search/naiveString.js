99c15d62f003fa6ae8515c6f1cb52238f93efc57
jthw1005 <skl0105@naver.com>
2022-09-22 14:52:56 +0900

    Solve naiveString.js

---
+++ udemy/Search/naiveString.js
@@ -0,0 +1,12 @@
+function naiveString(long, short) {
+  let cnt = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (long[i + j] !== short[j]) break;
+      if (j === short.length - 1) cnt++;
+    }
+  }
+  return cnt;
+}
+
+console.log(naiveString('this is tiger', 'is'));
