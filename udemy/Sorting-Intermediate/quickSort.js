b9307f21554a5d0e1d615c0e2a46f4676f23b5ac
jthw1005 <skl0105@naver.com>
2022-09-30 19:46:22 +0900

    Solve quickSort.js

---
+++ udemy/Sorting-Intermediate/quickSort.js
@@ -0,0 +1,29 @@
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    const idx = pivot(arr, left, right);
+    quickSort(arr, left, idx - 1);
+    quickSort(arr, idx + 1, right);
+  }
+  return arr;
+}
+
+const pivot = (arr, start = 0, end = arr.length - 1) => {
+  let swapIdx = start;
+  const pivot = arr[start];
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot >= arr[i]) {
+      swapIdx++;
+      swap(arr, i, swapIdx);
+    }
+  }
+
+  swap(arr, start, swapIdx);
+  return swapIdx;
+};
+
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
+console.log(quickSort([5, 4, 6, 8, 9, 12, 33, 44, 1]));
