db4c5cd7a45fdc5ca1986d7a39b683cfa6d26502
jthw1005 <skl0105@naver.com>
2022-10-04 16:43:09 +0900

    Solve binarySearchTree.js

---
+++ udemy/Trees/binarySearchTree.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (val > current.val) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  contains(val) {
+    if (!this.root) return false;
+    let current = this.root;
+    while (current) {
+      if (current.val === val) return true;
+      else if (current.val < val) {
+        current = current.right;
+      } else {
+        current = current.left;
+      }
+    }
+    return false;
+  }
+}
