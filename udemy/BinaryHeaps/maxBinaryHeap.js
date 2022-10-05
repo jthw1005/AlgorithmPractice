f0b13ebe00d4ee23b495b978a1c9aef8e761fc4b
jthw1005 <skl0105@naver.com>
2022-10-05 23:09:50 +0900

    Solve maxBinaryHeap.js

---
+++ udemy/BinaryHeaps/maxBinaryHeap.js
@@ -0,0 +1,76 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  insert(val) {
+    this.values.push(val);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let childIdx = this.values.length - 1;
+    while (childIdx > 0) {
+      const parentIdx = Math.floor((childIdx - 1) / 2);
+      if (this.values[childIdx] <= this.values[parentIdx]) break;
+      swap(this.values, childIdx, parentIdx);
+      childIdx = parentIdx;
+    }
+  }
+
+  extractMax() {
+    if (this.values.length <= 0) return null;
+
+    swap(this.values, 0, this.values.length - 1);
+    const max = this.values.pop();
+    this.sinkDown();
+
+    return max;
+  }
+
+  sinkDown() {
+    let parentIdx = 0;
+    const lastIdx = this.values.length - 1;
+
+    while (parentIdx < lastIdx) {
+      const childIdxs = [2 * parentIdx + 1, 2 * parentIdx + 2];
+      let targetIdx;
+      if (childIdxs[0] > lastIdx) break;
+      else if (childIdxs[0] === lastIdx) targetIdx = childIdxs[0];
+      else
+        targetIdx =
+          this.values[childIdxs[0]] > this.values[childIdxs[1]] ? childIdxs[0] : childIdxs[1];
+
+      if (this.values[parentIdx] >= this.values[targetIdx]) break;
+      swap(this.values, parentIdx, targetIdx);
+      parentIdx = targetIdx;
+    }
+  }
+}
+
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
+const heap = new MaxBinaryHeap();
+
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.insert(10);
+heap.insert(18);
+heap.insert(1);
+
+console.log(heap);
