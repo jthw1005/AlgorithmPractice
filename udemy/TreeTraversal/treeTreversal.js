d29227ae723f3611620bd00e627ce8e16128ecff
jthw1005 <skl0105@naver.com>
2022-10-05 18:28:14 +0900

    Solve treeTraversal.js

---
+++ udemy/TreeTraversal/treeTreversal.js
@@ -0,0 +1,71 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  bfs() {
+    const queue = [this.root];
+    const data = [];
+
+    while (queue.length) {
+      const tempNode = queue.shift();
+      data.push(tempNode.val);
+      // 이진 트리라서 if 문이 2개지만, 일반적인 트리인 경우에는
+      // 자식 노드들에 대해 루프를 돌려야 한다.
+      if (tempNode.left) queue.push(tempNode.left);
+      if (tempNode.right) queue.push(tempNode.right);
+    }
+
+    return data;
+  }
+
+  dfsPreOrder() {
+    const data = [];
+    const traverse = (node) => {
+      data.push(node.val);
+      // 이진 트리라서 if 문이 2개지만, 일반적인 트리인 경우에는
+      // 자식 노드들에 대해 루프를 돌려야 한다.
+      if (node.left) return traverse(node.left);
+      if (node.right) return traverse(node.right);
+    };
+
+    traverse(this.root);
+    return data;
+  }
+
+  dfsPostOrder() {
+    const data = [];
+    const traverse = (node) => {
+      // 이진 트리라서 if 문이 2개지만, 일반적인 트리인 경우에는
+      // 자식 노드들에 대해 루프를 돌려야 한다.
+      if (node.left) return traverse(node.left);
+      if (node.right) return traverse(node.right);
+      data.push(node.val);
+    };
+
+    traverse(this.root);
+    return data;
+  }
+
+  dfsInOrder() {
+    const data = [];
+    const traverse = (node) => {
+      // 이진 트리라서 if 문이 2개지만, 일반적인 트리인 경우에는
+      // 자식 노드들에 대해 루프를 돌려야 한다.
+      if (node.left) return traverse(node.left);
+      data.push(node.val);
+      if (node.right) return traverse(node.right);
+    };
+
+    traverse(this.root);
+    return data;
+  }
+}
