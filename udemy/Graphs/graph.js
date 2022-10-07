fd18b506f6c254dbaa08ac99953cb00646a5db83
jthw1005 <skl0105@naver.com>
2022-10-07 22:55:37 +0900

    Solve graph.js

---
+++ udemy/Graphs/graph.js
@@ -0,0 +1,29 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(name) {
+    if (!this.adjacencyList[name]) {
+      this.adjacencyList[name] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+
+  removeVertex(vertex) {
+    this.adjacencyList[vertex].forEach((el) => {
+      this.removeEdge(el, vertex);
+    });
+
+    delete this.adjacencyList[vertex];
+  }
+}
