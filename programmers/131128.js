d333ec9e689edd3fe7459364f0c8d5252928b24f
jthw1005 <skl0105@naver.com>
2022-10-12 23:10:32 +0900

    숫자 짝꿍 풀이 완료

---
+++ programmers/131128.js
@@ -0,0 +1,27 @@
+function solution(X, Y) {
+  const resultArr = [];
+  let answer;
+  const newX = X.split('').sort((a, b) => a - b);
+  const newY = Y.split('').sort((a, b) => a - b);
+
+  let x = 0,
+    y = 0;
+  while (x !== newX.length && y !== newY.length) {
+    if (newX[x] > newY[y]) y++;
+    else if (newX[x] < newY[y]) x++;
+    else {
+      resultArr.push(newX[x]);
+      x++;
+      y++;
+    }
+  }
+
+  if (resultArr.length === 0) {
+    answer = '-1';
+  } else if (resultArr.every((el) => +el === 0)) {
+    answer = '0';
+  } else {
+    answer = resultArr.sort((a, b) => b - a).join('');
+  }
+  return answer;
+}
