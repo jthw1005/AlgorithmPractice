2b02ac544f6b5b498fc8fba7abc7cb558bc020ac
jthw1005 <skl0105@naver.com>
2022-11-21 15:17:52 +0900

    1463 update

--- baekjoon/1463/solution.js
+++ baekjoon/1463/solution.js
@@ -1,17 +1,48 @@
+/* bottomup with iterative */
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// const input = +require('fs').readFileSync(filePath).toString().trim();
+
+// const dpArr = [null, 0, 1, 1];
+
+// for (let i = 4; i <= input; i++) {
+//   const arr = [dpArr[i - 1]];
+//   if (i % 3 === 0) {
+//     arr.push(dpArr[i / 3]);
+//   }
+//   if (i % 2 === 0) {
+//     arr.push(dpArr[i / 2]);
+//   }
+//   dpArr[i] = 1 + Math.min(...arr);
+// }
+
+// console.log(dpArr[input]);
+
+/* topdown with iterative*/
+
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = +require('fs').readFileSync(filePath).toString().trim();
 
-const dpArr = [null, 0, 1, 1];
+const mySet = new Set();
+let queue = [input];
+let cnt = 0;
 
-for (let i = 4; i <= input; i++) {
-  const arr = [dpArr[i - 1]];
-  if (i % 3 === 0) {
-    arr.push(dpArr[i / 3]);
+while (queue.length) {
+  const tempQueue = [];
+  for (let el of queue) {
+    if (el === 1) return console.log(cnt);
+    if (el % 3 === 0 && !mySet.has(el / 3)) {
+      tempQueue.push(el / 3);
+      mySet.has[el / 3] = true;
+    }
+    if (el % 2 === 0 && !mySet.has(el / 2)) {
+      tempQueue.push(el / 2);
+      mySet.has[el / 2] = true;
+    }
+    if (!mySet.has(el - 1)) {
+      tempQueue.push(el - 1);
+      mySet.has[el - 1] = true;
+    }
   }
-  if (i % 2 === 0) {
-    arr.push(dpArr[i / 2]);
-  }
-  dpArr[i] = 1 + Math.min(...arr);
+  cnt++;
+  queue = tempQueue;
 }
-
-console.log(dpArr[input]);
