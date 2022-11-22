5c74822ea1c72b270675b244e1b1faf7b19e7f9f
jthw1005 <skl0105@naver.com>
2022-11-22 19:14:07 +0900

    10816 done

---
+++ baekjoon/10816/solution.js
@@ -0,0 +1,19 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [n, arrN, m, arrM] = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const obj = {};
+arrN.split(' ').forEach((el) => {
+  obj[el] = obj[el] || 0;
+  obj[el]++;
+});
+
+const resultArr = [];
+arrM.split(' ').forEach((el, idx) => {
+  if (obj[el]) {
+    resultArr[idx] = obj[el];
+  } else {
+    resultArr[idx] = 0;
+  }
+});
+
+console.log(resultArr.join(' '));
