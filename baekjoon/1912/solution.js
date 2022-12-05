d02be7c6f4b7dd154a320f34895da6d07758de4d
jthw1005 <skl0105@naver.com>
2022-12-05 20:38:24 +0900

    1912 update

--- baekjoon/1912/solution.js
+++ baekjoon/1912/solution.js
@@ -4,7 +4,7 @@ const [n, input] = require('fs').readFileSync(filePath).toString().trim().split(
 const arr = input.split(' ').map(Number);
 const resultArr = [];
 for (let i = 0; i < arr.length; i++) {
-  resultArr.push(Math.max(arr[i], arr[i] + resultArr[resultArr.length - 1] || 0));
+  resultArr.push(Math.max(arr[i], arr[i] + resultArr[resultArr.length - 1] || -1000));
 }
 
 console.log(Math.max(...resultArr));
