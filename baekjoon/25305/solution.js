8cde53d54fbf2da8f2057c43555dbfee87279f53
jthw1005 <skl0105@naver.com>
2022-12-08 13:08:29 +0900

    25305 done

---
+++ baekjoon/25305/solution.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => v.split(' ').map(Number));
+
+console.log(input[1].sort((a, b) => b - a)[input[0][1] - 1]);
