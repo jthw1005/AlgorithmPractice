491327a73aab97184973f6d0222ab76810308850
jthw1005 <skl0105@naver.com>
2022-12-10 23:51:04 +0900

    update 2981

--- baekjoon/2981/solution.js
+++ baekjoon/2981/solution.js
@@ -26,31 +26,3 @@ for (let i = 2; i <= val; i++) {
   }
 }
 console.log(answer.join(' '));
-
-// const input = arr.sort((a, b) => a - b);
-// let answer = [];
-
-// const A = input[0];
-// const B = input[1];
-
-// let gcd = B - A;
-
-// const getGCD = (a, b) => (a % b ? getGCD(b, a % b) : b);
-
-// for (let i = 2; i < N; i++) {
-//   gcd = getGCD(gcd, input[i] - input[i - 1]);
-// }
-// answer.push(gcd);
-
-// for (let i = 2; i * i <= gcd; i++) {
-//   if (!(gcd % i)) {
-//     if (i === gcd / i) answer.push(i);
-//     else {
-//       answer.push(i);
-//       answer.push(gcd / i);
-//     }
-//   }
-// }
-// answer.push(gcd);
-// answer = Array.from(new Set(answer)).sort((a, b) => a - b);
-// console.log(answer.join(' '));
