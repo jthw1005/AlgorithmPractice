823e1d0a16007bffeed4ef2ab6c44aaac24e9696
jthw1005 <skl0105@naver.com>
2022-12-19 00:39:56 +0900

    4949 update

--- baekjoon/4949/solution.js
+++ baekjoon/4949/solution.js
@@ -4,3 +4,35 @@ const input = require('fs')
   .toString()
   .trim()
   .split('\n');
+
+const parenthesisPair = {
+  ')': '(',
+  '}': '{',
+  ']': '[',
+};
+const answer = [];
+
+const solution = (str) => {
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+      stack.push(str[i]);
+    } else if (str[i] === ')' || str[i] === '}' || str[i] === ']') {
+      const top = stack.pop();
+      if (top !== parenthesisPair[str[i]]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+};
+
+input.forEach((el) => {
+  if (el === '.') return;
+  const result = solution(el) ? 'yes' : 'no';
+  answer.push(result);
+});
+
+console.log(answer.join('\n'));
