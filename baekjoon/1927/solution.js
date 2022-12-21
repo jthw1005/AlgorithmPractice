dd80c38c2ce6f76c239601f1d90954865d631d49
jthw1005 <skl0105@naver.com>
2022-12-21 12:30:56 +0900

    1927 done

---
+++ baekjoon/1927/solution.js
@@ -0,0 +1,83 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [n, ...input] = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
+class Heap {
+  constructor() {
+    this.data = [];
+  }
+
+  insert(val) {
+    this.data.push(val);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let childIdx = this.data.length - 1;
+    while (childIdx > 0) {
+      const parentIdx = Math.floor((childIdx - 1) / 2);
+      if (this.data[parentIdx] <= this.data[childIdx]) {
+        break;
+      }
+      swap(this.data, childIdx, parentIdx);
+      childIdx = parentIdx;
+    }
+  }
+
+  extractMin() {
+    if (this.data.length < 1) return 0;
+    swap(this.data, 0, this.data.length - 1);
+    const min = this.data.pop();
+    this.sinkDown();
+    return min;
+  }
+
+  sinkDown() {
+    let parentIdx = 0;
+    const lastIdx = this.data.length - 1;
+    while (parentIdx < lastIdx) {
+      const [leftChildIdx, rightChildIdx] = [
+        2 * parentIdx + 1,
+        2 * parentIdx + 2,
+      ];
+      let targetIdx;
+      if (leftChildIdx > lastIdx) {
+        break;
+      } else if (leftChildIdx === lastIdx) {
+        targetIdx = leftChildIdx;
+      } else {
+        targetIdx =
+          this.data[leftChildIdx] < this.data[rightChildIdx]
+            ? leftChildIdx
+            : rightChildIdx;
+      }
+      if (this.data[targetIdx] >= this.data[parentIdx]) {
+        break;
+      }
+      swap(this.data, parentIdx, targetIdx);
+      parentIdx = targetIdx;
+    }
+  }
+}
+
+const myHeap = new Heap();
+const answer = [];
+
+input.forEach((el) => {
+  if (el === 0) {
+    const returnVal = myHeap.extractMin();
+    answer.push(returnVal);
+  } else {
+    myHeap.insert(el);
+  }
+});
+
+console.log(answer.join('\n'));
