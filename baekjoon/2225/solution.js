d6c813e8ac1e3be65ebf1b25fa90a1fe58ef2501
jthw1005 <skl0105@naver.com>
2023-02-09 15:24:56 +0900

    2225 update

--- baekjoon/2225/solution.js
+++ baekjoon/2225/solution.js
@@ -10,11 +10,8 @@ const dpArr = Array.from({ length: n + 1 }, () => new Array(k + 1).fill(1));
 
 for (let i = 1; i <= n; i++) {
   for (let j = 2; j <= k; j++) {
-    for (let m = 0; m <= i; m++) {
-      dpArr[i][j] += dpArr[m][j - 1] % 1000000000;
-    }
-    dpArr[i][j] -= 1;
+    dpArr[i][j] = (dpArr[i - 1][j] + dpArr[i][j - 1]) % 1000000000;
   }
 }
 
-console.log(dpArr[n][k] % 1000000000);
+console.log(dpArr[n][k]);
