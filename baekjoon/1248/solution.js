a3000e26fc17a2bb179d59eef6befc3e9d99442d
jthw1005 <skl0105@naver.com>
2023-03-03 10:43:32 +0900

    1249 done

---
+++ baekjoon/1248/solution.js
@@ -0,0 +1,33 @@
+const fp = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(fp).toString().trim().split('\n');
+const n = +input[0];
+const matrix = input[1].split('');
+
+// console.log(n, matrix);
+const newArr = [];
+
+for (let i = +n; i > 0; i--) {
+  newArr.push(matrix.splice(0, i));
+}
+
+const solution = (str, idx) => {
+  let candidate;
+  if (str[0] === '+') {
+    candidate = Array.from({ length: 10 }, (_, idx) => idx + 1);
+  } else if (str[0] === '-') {
+    candidate = Array.from({ length: 10 }, (_, idx) => -(idx + 1));
+  } else {
+    return 0;
+  }
+};
+
+solution('+', 0);
+solution('-', 0);
+
+/**
+ * 문자열의 첫 번째 요소가 양수이면 1 ~ 10
+ * 문자열의 첫 번째 요소가 양수이면 -1 ~ -10
+ * 문자열의 첫 번째 요소가 0이면 0
+ *
+ *
+ */
