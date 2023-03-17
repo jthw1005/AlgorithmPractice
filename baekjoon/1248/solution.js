41f30c7a74cc87228e48615ef6233ac0174957a3
jthw1005 <skl0105@naver.com>
2023-03-18 00:20:14 +0900

    1248 done

--- baekjoon/1248/solution.js
+++ baekjoon/1248/solution.js
@@ -3,23 +3,52 @@ const input = require('fs').readFileSync(fp).toString().trim().split('\n');
 const n = +input[0];
 const matrix = input[1].split('');
 
-// console.log(n, matrix);
 const newArr = [];
-
 for (let i = +n; i > 0; i--) {
   newArr.push(matrix.splice(0, i));
 }
 
-const solution = (str, idx) => {
-  let candidate;
-  if (str[0] === '+') {
-    candidate = Array.from({ length: 10 }, (_, idx) => idx + 1);
-  } else if (str[0] === '-') {
-    candidate = Array.from({ length: 10 }, (_, idx) => -(idx + 1));
-  } else {
-    return 0;
+const resultArr = [];
+
+const solution = (level) => {
+  // 종료 조건
+  if (level === n) {
+    console.log(resultArr.join(' '));
+    process.exit();
   }
+
+  // 다음 후보 선택
+  let candidate;
+  if (newArr[level][0] === '+') {
+    candidate = Array.from({ length: 10 }, (_, i) => i + 1);
+  } else if (newArr[level][0] === '-') {
+    candidate = Array.from({ length: 10 }, (_, i) => -(i + 1));
+  } else {
+    candidate = [0];
+  }
+
+  for (let i = 0; i < level; i++) {
+    candidate = candidate.filter((el) => {
+      let sum = el;
+      for (let j = i; j < level; j++) {
+        sum += resultArr[j];
+      }
+      if (newArr[i][level - i] === '+') {
+        return sum > 0;
+      } else if (newArr[i][level - i] === '-') {
+        return sum < 0;
+      } else {
+        return sum === 0;
+      }
+    });
+  }
+
+  // 실행
+  candidate.forEach((el) => {
+    resultArr.push(el);
+    solution(level + 1);
+    resultArr.pop();
+  });
 };
 
-solution('+', 0);
-solution('-', 0);
+solution(0);
