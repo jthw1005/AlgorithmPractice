6acec3d79e0b20a1a5f3794d053233b2e6b4566e
jthw1005 <skl0105@naver.com>
2023-03-31 23:10:20 +0900

    11758 done

---
+++ baekjoon/11758/solution.js
@@ -0,0 +1,15 @@
+const fp = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs')
+  .readFileSync(fp)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => v.split(' ').map(Number));
+
+const orientation = (p, q, r) => {
+  const val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1]);
+  if (val === 0) return 0;
+  return val > 0 ? -1 : 1;
+};
+
+console.log(orientation(input[0], input[1], input[2]));
