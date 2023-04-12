72c287643d66667e133c6a3f966442e7e4851a39
jthw1005 <skl0105@naver.com>
2023-04-13 01:55:32 +0900

    2887 update

--- baekjoon/2887/solution.js
+++ baekjoon/2887/solution.js
@@ -1,19 +1,23 @@
 const fp = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const [n, ...input] = require('fs')
+const [_, ...input] = require('fs')
   .readFileSync(fp)
   .toString()
   .trim()
   .split('\n')
-  .map((v) => v.split(' ').map(Number));
+  .map((v) => v.split(' ').map(Number))
+  .map((v, i) => {
+    v.push(i);
+    return v;
+  });
 
-const getParent = (parent, x) => {
+const findParent = (parent, x) => {
   if (parent[x] === x) return x;
-  return (parent[x] = getParent(parent, parent[x]));
+  return (parent[x] = findParent(parent, parent[x]));
 };
 
 const unionParent = (parent, a, b) => {
-  a = getParent(parent, a);
-  b = getParent(parent, b);
+  a = findParent(parent, a);
+  b = findParent(parent, b);
 
   if (a < b) {
     parent[b] = a;
@@ -23,8 +27,9 @@ const unionParent = (parent, a, b) => {
 };
 
 const kruskalAlgorithm = (edges, n) => {
-  let parent = [];
+  const parent = [];
   let result = 0;
+  let cost;
 
   for (let i = 1; i <= n; i++) {
     parent[i] = i;
@@ -33,14 +38,10 @@ const kruskalAlgorithm = (edges, n) => {
   edges.sort((a, b) => a.cost - b.cost);
 
   for (let edge of edges) {
-    let cost = edge.cost;
-    let a = edge.a;
-    let b = edge.b;
-
-    if (getParent(parent, a) !== getParent(parent, b)) {
-      unionParent(parent, a, b);
-      result += cost;
-    }
+    cost = edge.cost;
+    if (findParent(parent, edge.a) === findParent(parent, edge.b)) continue;
+    unionParent(parent, edge.a, edge.b);
+    result += cost;
   }
 
   return result;
@@ -48,17 +49,16 @@ const kruskalAlgorithm = (edges, n) => {
 
 const minCostTunnel = (coordinates) => {
   let n = coordinates.length;
-  let edges = [];
+  const edges = [];
 
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      let cost = Math.min(
-        Math.abs(coordinates[i][0] - coordinates[j][0]),
-        Math.abs(coordinates[i][1] - coordinates[j][1]),
-        Math.abs(coordinates[i][2] - coordinates[j][2])
-      );
-
-      edges.push({ a: i + 1, b: j + 1, cost: cost });
+  for (let i = 0; i < 3; i++) {
+    coordinates.sort((n, p) => n[i] - p[i]);
+    for (let j = 0; j < n - 1; j++) {
+      edges.push({
+        a: coordinates[j][3],
+        b: coordinates[j + 1][3],
+        cost: Math.abs(coordinates[j][i] - coordinates[j + 1][i]),
+      });
     }
   }
 
