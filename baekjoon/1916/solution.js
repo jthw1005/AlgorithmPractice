6cbdd01da97478106868852ef38b8fe0e91e0287
jthw1005 <skl0105@naver.com>
2023-06-08 00:49:44 +0900

    update 1916

--- baekjoon/1916/solution.js
+++ baekjoon/1916/solution.js
@@ -1,14 +1,15 @@
 const fp = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(fp).toString().trim().split('\n');
 
-const N = parseInt(input[0]);
-const M = parseInt(input[1]);
-const [start, end] = input[M + 2].split(' ').map((e) => parseInt(e));
+const N = +input[0];
+const M = +input[1];
+const [start, end] = input[M + 2].split(' ').map(Number);
 
 const graph = Array.from(Array(N + 1), () => []);
+const visited = Array.from(Array(N + 1), () => false);
 
 for (let i = 2; i < M + 2; i++) {
-  const [from, to, cost] = input[i].split(' ').map((e) => parseInt(e));
+  const [from, to, cost] = input[i].split(' ').map(Number);
   graph[from].push([to, cost]);
 }
 
@@ -18,14 +19,16 @@ function dijkstra(start) {
   distances[start] = 0;
   while (queue.length) {
     const [currentNode, currentCost] = queue.pop();
+    visited[currentNode] = true;
     if (distances[currentNode] < currentCost) continue;
     for (const [nextNode, nextCost] of graph[currentNode]) {
       const cost = currentCost + nextCost;
-      if (cost < distances[nextNode]) {
+      if (cost < distances[nextNode] && !visited[nextNode]) {
         distances[nextNode] = cost;
         queue.push([nextNode, cost]);
       }
     }
+    queue.sort((n, p) => p[1] - n[1]);
   }
   return distances;
 }
