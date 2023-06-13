7270d3311da624ca3ffc8851f6c2807f7dc29331
jthw1005 <skl0105@naver.com>
2023-06-14 08:45:28 +0900

    update 1005

--- baekjoon/1005/solution.js
+++ baekjoon/1005/solution.js
@@ -4,39 +4,29 @@ const T = +input.splice(0, 1);
 
 const solution = (N, K, order, times, dest) => {
   const graph = Array.from({ length: N + 1 }, () => []);
-  const reverseGraph = Array.from({ length: N + 1 }, () => []);
-  const indegree = Array.from({ length: N }, () => 0);
-  const dpTable = Array.from({ length: N + 1 }, () => 0);
+  const indegree = Array.from({ length: N + 1 }, () => 0);
+  const dpTable = [0, ...times];
   const queue = [];
 
   for (const [start, end] of order) {
     graph[start].push(end);
-    reverseGraph[end].push(start);
-    indegree[end - 1]++;
+    indegree[end]++;
   }
 
-  for (let i = 0; i < N; i++) {
+  for (let i = 1; i <= N; i++) {
     if (indegree[i] === 0) {
-      queue.push(i + 1);
+      queue.push(i);
     }
   }
 
   while (queue.length) {
     const curr = queue.shift();
-    let max = -Infinity;
     for (let el of graph[curr]) {
-      indegree[el - 1]--;
-      if (indegree[el - 1] === 0) {
+      indegree[el]--;
+      if (indegree[el] === 0) {
         queue.push(el);
       }
-    }
-
-    if (reverseGraph[curr].length === 0) {
-      dpTable[curr] = times[curr - 1];
-    } else {
-      dpTable[curr] =
-        Math.max(...reverseGraph[curr].map((v) => dpTable[v])) +
-        times[curr - 1];
+      dpTable[el] = Math.max(dpTable[el], dpTable[curr] + times[el - 1]);
     }
   }
 
