4103816fc3568b3ff5888cce1f85a18cb867e33c
jthw1005 <skl0105@naver.com>
2023-07-02 00:06:27 +0900

    update 1238

--- baekjoon/1238/solution.js
+++ baekjoon/1238/solution.js
@@ -1,30 +1,44 @@
 const fp = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs')
+  .readFileSync(fp)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => v.split(' ').map(Number));
 
-const input = require('fs').readFileSync(fp).toString().trim().split('\n');
+const [n, m, x] = input[0];
 
-const [N, M, X] = input[0].split(' ').map(Number);
-const graph = Array.from({ length: N + 1 }, () => Array(N + 1).fill(Infinity));
+const graph = Array.from({ length: n + 1 }, () =>
+  new Array(n + 1).fill(Infinity)
+);
 
-for (let i = 1; i <= N; i++) {
-  graph[i][i] = 0;
+for (let i = 1; i <= m; i++) {
+  const [start, end, dist] = input[i];
+  graph[start][end] = dist;
 }
 
-for (let i = 1; i <= M; i++) {
-  const [from, to, time] = input[i].split(' ').map(Number);
-  graph[from][to] = time;
-}
-
-for (let k = 1; k <= N; k++) {
-  for (let i = 1; i <= N; i++) {
-    for (let j = 1; j <= N; j++) {
-      graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+for (let k = 1; k <= n; k++) {
+  for (let i = 1; i <= n; i++) {
+    if (i === k) continue;
+    for (let j = 1; j <= n; j++) {
+      if (j === k) continue;
+      if (graph[i][k] === Infinity || graph[k][j] === Infinity) continue;
+      const newDist = graph[i][k] + graph[k][j];
+      if (graph[i][j] > newDist) {
+        graph[i][j] = newDist;
+      }
     }
   }
 }
 
-let maxTime = -1;
-for (let i = 1; i <= N; i++) {
-  maxTime = Math.max(maxTime, graph[i][X] + graph[X][i]);
+let answer = 0;
+
+for (let i = 1; i <= n; i++) {
+  if (i === x) continue;
+  const candidate = graph[i][x] + graph[x][i];
+  if (candidate > answer) {
+    answer = candidate;
+  }
 }
 
-console.log(maxTime);
+console.log(answer);
