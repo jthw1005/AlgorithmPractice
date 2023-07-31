7d8caeef87e1858cc12d31ec92ecea3d5733a874
jthw1005 <skl0105@naver.com>
2023-07-31 23:41:47 +0900

    27172 done

--- baekjoon/27172/solution.js
+++ baekjoon/27172/solution.js
@@ -4,26 +4,28 @@ const input = require('fs').readFileSync(fp).toString().trim().split('\n');
 const N = +input[0];
 const numbers = input[1].split(' ').map(Number);
 
-const scores = new Array(1_000_001).fill(0);
-const cards = new Array(1_000_001).fill(0);
-
 const solution = (N, numbers) => {
+    const max = Math.max(...numbers);
+
+    const scores = new Array(max).fill(0);
+    const cards = new Array(max).fill(0);
+
     for (let i = 0; i < N; i++) {
-        cards[numbers[i]] = 1;
+        cards[numbers[i] - 1] = 1;
     }
 
     for (let i = 0; i < N; i++) {
-        for (let j = numbers[i] * 2; j < 1_000_001; j += numbers[i]) {
-            if (cards[j]) {
-                scores[numbers[i]]++;
-                scores[j]--;
+        for (let j = numbers[i] * 2; j <= max; j += numbers[i]) {
+            if (cards[j - 1]) {
+                scores[numbers[i] - 1]++;
+                scores[j - 1]--;
             }
         }
     }
 
     let output = '';
     for (let i = 0; i < N; i++) {
-        output += scores[numbers[i]] + ' ';
+        output += scores[numbers[i] - 1] + ' ';
     }
     console.log(output.trim());
 };
