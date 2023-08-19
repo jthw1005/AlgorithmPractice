7914330937fcb4db8977752ef832c9a9c0f60294
jthw1005 <skl0105@naver.com>
2023-08-19 23:27:38 +0900

    7579 done

---
+++ baekjoon/7579/solution.js
@@ -0,0 +1,27 @@
+const fp = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [nM, memories, costs] = require('fs')
+  .readFileSync(fp)
+  .toString()
+  .trim()
+  .split('\n')
+  .map(line => line.split(' ').map(Number));
+const [N, M] = nM;
+const MAX_COST = N * 100;
+
+let dp = new Array(MAX_COST + 1).fill(0);
+
+for (let i = 0; i < N; i++) {
+  for (let j = MAX_COST; j >= costs[i]; j--) {
+    dp[j] = Math.max(dp[j], dp[j - costs[i]] + memories[i]);
+  }
+}
+
+let answer = MAX_COST;
+for (let i = 0; i <= MAX_COST; i++) {
+  if (dp[i] >= M) {
+    answer = i;
+    break;
+  }
+}
+
+console.log(answer);
