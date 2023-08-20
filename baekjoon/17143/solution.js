6407a839f5a543471dca17966747ea031120f2a0
jthw1005 <skl0105@naver.com>
2023-08-20 19:06:02 +0900

    update 17143

--- baekjoon/17143/solution.js
+++ baekjoon/17143/solution.js
@@ -6,25 +6,41 @@ const [[mapRow, mapCol, numOfSharks], ...sharkData] = require('fs')
   .split('\n')
   .map(v => v.split(' ').map(Number));
 
+function calNextPos(border, curPos, direction, speed) {
+  let rotateCnt = 0;
+  let d;
+
+  if (direction === 1 || direction === 4) d = -1;
+  else d = 1;
+
+  while (speed > 0) {
+    if ((curPos === 1 && d === -1) || (curPos === border && d === 1)) {
+      d = d * -1;
+      rotateCnt++;
+    }
+    curPos = curPos + d;
+    speed--;
+  }
+
+  return [curPos, !!(rotateCnt % 2)];
+}
+
 function solution(mapRow, mapCol, numOfSharks, sharkData) {
   let amountOfSharks = 0;
 
-  // sharkData 수정
-  sharkData = sharkData.map((v, i) => [...v, i + 1]);
+  sharkData = sharkData.map(([r, c, s, d, z], i) => {
+    return [r, c, d < 3 ? s % ((mapRow - 1) * 2) : s % ((mapCol - 1) * 2), d, z, i + 1];
+  });
 
-  // map 초기화
   let map = Array.from({ length: mapRow + 1 }, () => new Array(mapCol + 1).fill(null));
   for (const [row, col, speed, direction, size, name] of sharkData) {
     map[row][col] = name;
   }
 
-  // 낚시왕이 1에 위치하고 게임 시작
   for (let fisherManCol = 1; fisherManCol <= mapCol; fisherManCol++) {
-    // 맵 내 낚시꾼이 위치한 열에서 땅에 가장 가까운 상어 포획
     for (let i = 1; i <= mapRow; i++) {
       let caughtSharkName = map[i][fisherManCol];
       if (caughtSharkName) {
-        // sharkData에서 해당 name 찾고 size 추가 및 제거
         for (let j = 0; j < sharkData.length; j++) {
           if (sharkData[j][5] === caughtSharkName) {
             amountOfSharks += sharkData[j][4];
@@ -38,60 +54,28 @@ function solution(mapRow, mapCol, numOfSharks, sharkData) {
 
     map = Array.from({ length: mapRow + 1 }, () => new Array(mapCol + 1).fill(null));
 
-    // 상어 별 이동 처리
     for (let i = 0; i < sharkData.length; i++) {
       const [row, col, speed, direction, size, name] = sharkData[i];
-      if (!name) {
-        continue;
-      }
+      if (!name) continue;
 
       if (direction === 1) {
-        // up
-        const newSpeed = speed % ((mapRow - 1) * 2);
-        if (newSpeed <= row - 1) {
-          sharkData[i][0] = row - newSpeed;
-        } else if (newSpeed <= row - 1 + mapRow - 1) {
-          sharkData[i][0] = 1 + newSpeed - row + 1;
-          sharkData[i][3] = 2;
-        } else {
-          sharkData[i][0] = mapRow - (newSpeed - row + 1 - mapRow + 1);
-        }
+        const [newPos, isRotate] = calNextPos(mapRow, row, direction, speed);
+        sharkData[i][0] = newPos;
+        if (isRotate) sharkData[i][3] = 2;
       } else if (direction === 2) {
-        // down
-        const newSpeed = speed % ((mapRow - 1) * 2);
-        if (newSpeed <= mapRow - row) {
-          sharkData[i][0] = row + newSpeed;
-        } else if (newSpeed <= mapRow - row + mapRow - 1) {
-          sharkData[i][0] = mapRow - newSpeed + mapRow - row;
-          sharkData[i][3] = 1;
-        } else {
-          sharkData[i][0] = 1 + newSpeed - mapRow + row - mapRow + 1;
-        }
+        const [newPos, isRotate] = calNextPos(mapRow, row, direction, speed);
+        sharkData[i][0] = newPos;
+        if (isRotate) sharkData[i][3] = 1;
       } else if (direction === 3) {
-        // right
-        const newSpeed = speed % ((mapCol - 1) * 2);
-        if (newSpeed <= mapCol - col) {
-          sharkData[i][1] = col + newSpeed;
-        } else if (newSpeed <= mapCol - col + mapCol - 1) {
-          sharkData[i][1] = mapCol - newSpeed + mapCol - col;
-          sharkData[i][3] = 4;
-        } else {
-          sharkData[i][1] = 1 + newSpeed - mapCol + col - mapCol + 1;
-        }
+        const [newPos, isRotate] = calNextPos(mapCol, col, direction, speed);
+        sharkData[i][1] = newPos;
+        if (isRotate) sharkData[i][3] = 4;
       } else {
-        // left
-        const newSpeed = speed % ((mapCol - 1) * 2);
-        if (newSpeed <= col - 1) {
-          sharkData[i][1] = col - newSpeed;
-        } else if (newSpeed <= col - 1 + mapCol - 1) {
-          sharkData[i][1] = 1 + newSpeed - col + 1;
-          sharkData[i][3] = 3;
-        } else {
-          sharkData[i][1] = mapCol - (newSpeed - col + 1 - mapCol + 1);
-        }
+        const [newPos, isRotate] = calNextPos(mapCol, col, direction, speed);
+        sharkData[i][1] = newPos;
+        if (isRotate) sharkData[i][3] = 3;
       }
 
-      // map과 동기화
       const sharkName = map[sharkData[i][0]][sharkData[i][1]];
       if (sharkName) {
         for (let j = 0; j < sharkData.length; j++) {
